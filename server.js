d17613a2ded367a26708a0831b138755102dff67
GREATBIGNATE <greatbignate@gmail.com>
2018-09-25 09:36:33 -0700

    initial populate directory level server.js

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const express  require('express');
+const superagent = require('superagent');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+require('dotenv').config();
+
+app.use(express.static('.public/styles')); //Was this right?
+app.use(express.urlencoded({extended: true}));
+
+app.set('view engine', 'ejs');
+
+app.get('/', (request, response) => {
+  response.render('index');
+})
+
+
+
+
+
+
+
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
