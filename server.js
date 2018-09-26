c2f8ea67cd4739152faf308ce6e8d2dedf2a67d0
nguye <nguyenv2@outlook.com>
2018-09-26 10:58:52 -0700

    added detail pages

--- server.js
+++ server.js
@@ -4,19 +4,49 @@ const express = require('express');
 const superagent = require('superagent');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const pg = require('pg');
+const client = new pg.Client('postgres://postgres:hacker@localhost:5432/books_app');
+client.connect();
+client.on('error', err => console.error(err));
 
 require('dotenv').config();
-
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public')); //Was this right?
 
 app.set('view engine', 'ejs');
 
-app.get('/', (request, response) => {
-  response.render('pages/index');
-})
+app.get('/', getBooks);
+
+app.get('/books/:book_id', getBookDetail);
 
 app.post('/searches', searchBook);
+
+function getBookDetail(request, response) {
+  let SQL = 'SELECT * FROM books WHERE id=$1;';
+  let values = [request.params.book_id];
+  console.log(values);
+
+  return client.query(SQL, values)
+    .then(result => {
+      console.log(result);
+      response.render('pages/detail-view', { book: result.rows[0] });
+
+    })
+    .catch(handleError);
+}
+
+
+
+
+
+
+function getBooks(request, response) {
+  let SQL = 'SELECT * from books;';
+
+  return client.query(SQL)
+    .then(results => response.render('pages/index', { results: results.rows }))
+    .catch(handleError);
+}
 
 function searchBook(request,response){
   let url = 'https://www.googleapis.com/books/v1/volumes?q=';
