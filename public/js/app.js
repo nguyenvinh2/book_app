bde98588ed89494b8e2327273f3a2ff6191e37ad
nguye <nguyenv2@outlook.com>
2018-09-27 13:23:04 -0700

    MVP?

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,7 @@
 
 $(document).ready(function () {
   $('.create-view-container').hide();
+  $('.book-view-update').hide();
 });
 
 $('button').click(function () {
@@ -10,7 +11,18 @@ $('button').click(function () {
   $(`.button-id.${myClass}`).hide();
 });
 
+$('button').click(function () {
+  if ($(this).attr('class').split(' ')[0] === 'create-view-container') {
+    const hideForm = $(this).attr('class').split(' ')[0];
+    const showButtons = $(this).attr('class').split(' ')[1];
+    $(`.${hideForm}`).hide();
+    $(`.button-id.${showButtons}`).show();
+  }
+});
 
-//function enterForm() {
-
-//}
+$('button').click(function () {
+  if ($(this).attr('class') === 'edit') {
+    $('.book-view-update').toggle();
+    $('.book-view.container').toggle();
+  }
+});
